a14f8d29da027187c8a315d948370a9bf9eeb88f
niyu521 <ynny.1625@gmail.com>
2025-01-20 11:35:23 +0900

    Auto-commit: changes made at 2025-01-20 11:35:22

--- alias.sh
+++ alias.sh
@@ -5,13 +5,15 @@ alias cc='code .'
 alias cs='cursor'
 alias csc='cursor .'
 
+alias o='open'
+alias oc='open .'
 
 alias l='cd ~/Documents/02_Code/document/univ_class && ls'
 alias zho='code ~/my-zsh-config && code -r ~/.zshrc'
 alias zhs='source ~/.zshrc && commit_and_push'
 alias cb='cd ..'
 alias codi='cd ~/Documents/02_Code'
-alias o='open .'
+
 alias mft='cd ~/Documents/02_Code/project/matsufes2024_test'
 alias maf='cd ~/Documents/02_Code/project/Matsufes2024'
 alias write='cat >'
